fd81fcdc3522c09b77878cb9c404e9e2232b6e40
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2019-12-03 20:00:08 +0900

    Update groups_controller.rb
    corresponding action in the controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,31 @@
 class GroupsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if current_user.create(group_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(group_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
 end
