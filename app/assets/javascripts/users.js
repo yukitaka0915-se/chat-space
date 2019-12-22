24db5330da8d587af0e2136b723d5e986632a4e2
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2019-12-22 15:05:39 +0900

    add group menber function [add button]
    SSIA

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,17 +1,19 @@
 $(function(){
-
+  //ユーザーリストの親要素を取得
   let user_list = $('#user-search-result');
-  
-  function appendUser(user) {
+
+  //ユーザーリストの追加用HTMLの生成
+  function appendUser(user_name, user_id) {
     let html = `
                 <div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                  <p class="chat-group-user__name">${user_name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user_id}" data-user-name="${user_name}">追加</div>
                 </div>
                 `
     user_list.append(html);
   }
   
+  //ユーザーが見つからなかった時の追加用HTMLの生成
   function appendErrMsgToHTML(msg) {
     let html = `
                <div class="chat-group-user clearfix">
@@ -20,6 +22,7 @@ $(function(){
     user_list.append(html);
   }
 
+  // チャットメンバー検索テキストでのキー入力イベント処理
   $('#user-search-field').on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -32,7 +35,7 @@ $(function(){
       user_list.empty();
       if (users.length !== 0) {
         users.forEach(function(user){
-          appendUser(user);
+          appendUser(user.name, user.id);
         });
       }
       else {
@@ -42,5 +45,42 @@ $(function(){
     .fail(function() {
       alert("ユーザー検索に失敗しました");
     })
-  });    
+  });
+  
+  function  appendMember(user_name, user_id){
+    var html = `
+            <div class='chat-group-user'>
+              <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+              <p class='chat-group-user__name'>${user_name}</p>
+              <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+            </div>
+            `
+    $('#chat-group-users').append(html)
+  }
+
+  // チャットメンバー候補の追加ボタンclick処理
+  $('#user-search-result').on("click", '.chat-group-user__btn--add', function() {
+    //追加ボタンが押された要素のデータを取得
+    let user_id = $(this).attr('data-user-id');
+    let user_name = $(this).attr('data-user-name');
+    //追加ボタンが押された親要素を削除
+    $(this).parent()[0].remove();
+    //追加ボタンが押された要素をチャットメンバーに追加
+    appendMember(user_name, user_id);
+  })
+  
+  // // チャットメンバー一覧の削除ボタンclick処理
+  // $('#chat-group-users').on("click", '.js-remove-btn', function() {
+  //   //追加ボタンが押された要素のデータを取得
+  //   console.log($(this).attr('[name="group[user_ids][]"]').val());
+  //   // let user_id = $(this).children().attr('[name="group[user_ids][]"]').val();
+  //   // let user_name = $('.chat-group-user__name').val();
+  //   // console.log(user_id);
+  //   // console.log(user_name);
+  //   //追加ボタンが押された親要素を削除
+  //   // $(this).parent()[0].remove();
+  //   //追加ボタンが押された要素をチャットメンバーに追加
+  //   // appendUser(user_name, user_id);
+  // })
+
 });
